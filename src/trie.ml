4ccab9fc265a44b73fad638face05346c75f3a1a
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2014-08-05 15:26:16 -0400

    added remove, and fixed insert & mem

--- src/trie.ml
+++ src/trie.ml
@@ -2,8 +2,11 @@ module Trie : sig
   type 'a trie
 
   val empty : 'a trie
-  val insert : 'a trie -> string -> 'a -> 'a trie
   val find : 'a trie -> string -> 'a
+  val mem : 'a trie -> string -> bool
+  val insert : 'a trie -> string -> 'a -> 'a trie
+  val remove : 'a trie -> string -> 'a trie
+(*   val fold : 'a trie -> (char -> 'a option -> 'b -> 'b) -> 'b -> 'b *)
 
 end = struct
   module CharMap = Map.Make(Char)
@@ -20,6 +23,14 @@ end = struct
       | Trie(_, map) -> aux (i+1) (CharMap.find s.[i] map)
     in aux 0 t 
 
+  let mem : 'a trie -> string -> bool = fun t s ->
+    let l = String.length s in
+    let rec aux i = function
+      | Trie(Some v, _) when i = (l-1) -> true
+      | Trie(None, _) when i = (l-1) -> false
+      | Trie(_, map) -> try aux (i+1) (CharMap.find s.[i] map) with Not_found -> false
+    in aux 0 t   
+
   let insert : 'a trie -> string -> 'a -> 'a trie = fun t s x ->
     let l = String.length s in
     let rec aux cont i = function
@@ -28,14 +39,21 @@ end = struct
           let t' = try
             CharMap.find s.[i] map
           with | Not_found -> empty in
-          aux (fun x -> Trie(v, CharMap.add (s.[i]) x map)) (i+1) t'
+          aux (fun x -> cont(Trie(v, CharMap.add (s.[i]) x map))) (i+1) t'
     in aux (fun x -> x) 0 t
 
-     (*  try 
-        Trie(v, CharMap.add (s.[i]) (aux (i+1) (CharMap.find s.[i] map)) map)
-      with Not_found ->
-        let t' = aux (i+1) empty in
-        Trie(v, CharMap.add (s.[i]) t' map) *)
+  let remove : 'a trie -> string -> 'a trie = fun t s ->
+    let l = String.length s in
+    let rec aux (cont : 'a trie -> 'a trie) i = function
+      | Trie(v, map) when i = (l-1) -> cont (Trie(None, map))
+      | Trie(v, map) ->
+          aux (fun x -> cont (Trie(v, CharMap.add (s.[i]) x map))) (i+1) (CharMap.find (s.[i]) map)
+    in aux (fun x -> x) 0 t
+
+(*     let rec fold : 'a trie -> (char -> 'a option -> 'b -> 'b) -> 'b -> 'b =
+    fun Trie(v, map) f init ->  *)
+    
+
 end
 
 module Make : 
@@ -44,9 +62,10 @@ module Make :
 
     val empty : 'a trie
     val find : 'a trie -> Ord.t array -> 'a
+    val mem : 'a trie -> Ord.t array -> bool
     val insert : 'a trie -> Ord.t array -> 'a -> 'a trie
-  end
-  = functor (Ord : Map.OrderedType) -> struct
+  end = 
+  functor (Ord : Map.OrderedType) -> struct
 		module OrdMap = Map.Make(Ord)
 
 		type 'a trie = Trie of 'a option * ('a trie) OrdMap.t
@@ -54,22 +73,37 @@ module Make :
 		let empty : 'a trie = Trie(None, OrdMap.empty)
 
 		let find : 'a trie -> Ord.t array -> 'a = fun t s ->
-		    let l = Array.length s in
-		    let rec aux i = function
-		      | Trie(Some v, _) when i = (l-1) -> v
-		      | Trie(None, _) when i = (l-1) -> raise Not_found
-		      | Trie(_, map) -> aux (i+1) (OrdMap.find s.(i) map)
-		    in aux 0 t
+	    let l = Array.length s in
+	    let rec aux i = function
+	      | Trie(Some v, _) when i = (l-1) -> v
+	      | Trie(None, _) when i = (l-1) -> raise Not_found
+	      | Trie(_, map) -> aux (i+1) (OrdMap.find s.(i) map)
+	    in aux 0 t
+
+    let mem : 'a trie -> Ord.t array -> bool = fun t s ->
+      let l = Array.length s in
+      let rec aux i = function
+        | Trie(Some v, _) when i = (l-1) -> true
+        | Trie(None, _) when i = (l-1) -> false
+        | Trie(_, map) -> try aux (i+1) (OrdMap.find s.(i) map) with Not_found -> false
+      in aux 0 t
 
 		let insert : 'a trie -> Ord.t array -> 'a -> 'a trie = fun t s x ->
-		    let l = Array.length s in
-		    let rec aux cont i = function
-		      | Trie(v, map) when i = (l-1) -> cont (Trie(Some x, map))
-		      | Trie(v, map) ->
-      		  let t' = try
-              OrdMap.find s.(i) map
-            with | Not_found -> empty in
-            aux (fun x -> Trie(v, OrdMap.add (s.(i)) x map)) (i+1) t'
-		    in aux (fun x -> x) 0 t
+	    let l = Array.length s in
+	    let rec aux cont i = function
+	      | Trie(v, map) when i = (l-1) -> cont (Trie(Some x, map))
+	      | Trie(v, map) ->
+    		  let t' = try
+            OrdMap.find s.(i) map
+          with | Not_found -> empty in
+          aux (fun x -> cont(Trie(v, OrdMap.add (s.(i)) x map))) (i+1) t'
+	    in aux (fun x -> x) 0 t
 
+    let remove : 'a trie -> Ord.t array -> 'a trie = fun t s ->
+      let l = Array.length s in
+      let rec aux cont i = function
+        | Trie(v, map) when i = (l-1) -> cont (Trie(None, map))
+        | Trie(v, map) ->
+            aux (fun x -> cont(Trie(v, OrdMap.add (s.(i)) x map))) (i+1) (OrdMap.find (s.(i)) map)
+      in aux (fun x -> x) 0 t
 end
